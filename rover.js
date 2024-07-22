c72aff4858d32a2791708cd00ddf3e5b7c1d860e
Bailey Johnson <bailey506@hotmail.com>
2024-07-22 10:28:24 -0500

    Mars Rover Graded Assignment 3

--- rover.js
+++ rover.js
@@ -1,5 +1,44 @@
 class Rover {
-   // Write code here!
+   constructor(position) {
+      this.position = position;
+      this.mode = 'NORMAL';
+      this.generatorWatts = 110;
+   }
+
+   receiveMessage(message) {
+      let results = [];
+      for (let command of message.commands) {
+         let result = { completed: true };
+
+         if (command.commandType === 'STATUS_CHECK') {
+            result.roverStatus = {
+               mode: this.mode,
+               generatorWatts: this.generatorWatts,
+               position: this.position
+            };
+
+         } else if (command.commandType === 'MODE_CHANGE') {
+            this.mode = command.value;
+         } else if (command.commandType === 'MOVE') {
+            if (this.mode === 'LOW_POWER') {
+               result.completed = false;
+            } else {
+               this.position = command.value;
+            }
+         }
+
+         results.push(result);
+
+      }
+      return {
+         message: message.name,
+         results: results
+      };
+
+   }
+
+
 }
+
 
 module.exports = Rover;
--- spec/message.spec.js
+++ spec/message.spec.js
@@ -4,6 +4,21 @@ const Command = require('../command.js');
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
-describe("Message class", function() {
+describe('Message class', function() {
 
+    it('throws error if a name is NOT passed into the constructor as the first parameter', function() {
+        expect(function() {new Message();}).toThrow(new Error('Name is required'));
+    });
+
+    it('constructor sets name', function() {
+        const name = 'Test message';
+        const message = new Message(name);
+        expect(message.name).toEqual(name);
+    });
+
+    it('contains a commands array passed into the constructor as 2nd argument', function() {
+        const commands = [new Command('MODE CHANGE', 'LOW POWER'), new Command('STATUS_CHECK')];
+        const message = new Message('Test message', commands);
+        expect(message.commands).toEqual(commands);
+    });
 });
